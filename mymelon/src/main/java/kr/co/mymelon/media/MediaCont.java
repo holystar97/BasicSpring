aff225e7ef9167e22881ee5379d69b16b34fceaf
holystar97 <mkrice500@gmail.com>
2020-02-07 18:11:01 +0900

    delete

---
+++ mymelon/src/main/java/kr/co/mymelon/media/MediaCont.java
@@ -0,0 +1,11 @@
+package kr.co.mymelon.media;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MediaCont {
+	public MediaCont() {
+		System.out.println(" -- media cont  객체 생성");
+	}
+	
+}
